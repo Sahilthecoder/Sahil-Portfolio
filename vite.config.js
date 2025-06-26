ed336a039fbd4e56c695cfa8d447476d2db497fb
Sahil <sahilkhan36985@gmail.com>
2025-06-27 02:52:20 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Sahil-portfolio/',
   plugins: [react()],
@@ -23,5 +22,9 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
     emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
   },
 });

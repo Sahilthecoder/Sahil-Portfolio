94d7d80465e06287d246d4e18b5c0eb6e6616d39
Sahil <sahilkhan36985@gmail.com>
2025-07-18 10:45:56 +0530

    Fix PostCSS configuration for build

--- postcss.config.js
+++ postcss.config.js
@@ -2,42 +2,11 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
-    'tailwindcss': {},
-    'autoprefixer': {
+    tailwindcss: {},
+    autoprefixer: {
       flexbox: 'no-2009',
       grid: 'autoplace'
     },
-    'postcss-flexbugs-fixes': {},
-    ...(process.env.NODE_ENV === 'production' ? {
-      '@fullhuman/postcss-purgecss': {
-        content: [
-          './src/**/*.{js,jsx,ts,tsx,html}',
-          './public/index.html',
-        ],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: {
-          standard: [
-            'html', 'body', 'active', 'dark', 'light',
-            /-(leave|enter|appear)(|-(to|from|active))$/,
-            /^(?!(|.*?:)cursor-move).+-move$/,
-            /^router-link(|-exact)-active$/,
-            /data-v-.*/,
-          ],
-          deep: [
-            /^v-/, /^vs-/, /^vs-/, /^vgt-/, /^__/,
-            /^theme-/, /^data-theme$/,
-            /^vs-theme-/, /^vs-con-/, /^vs-sidebar-/, /^vs-*/,
-          ],
-        },
-      },
-      'cssnano': {
-        preset: ['default', {
-          discardComments: {
-            removeAll: true,
-          },
-          normalizeWhitespace: true,
-        }],
-      }
-    } : {})
-  },
+    'postcss-flexbugs-fixes': {}
+  }
 };
